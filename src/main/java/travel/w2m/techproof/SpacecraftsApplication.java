4abe5701655e8bd7a3a84d5e9722fcaefe5a8535
jd-ap <jesidacosta@gmail.com>
2024-10-19 15:10:54 +0200

    initial commit

---
+++ src/main/java/travel/w2m/techproof/SpacecraftsApplication.java
@@ -0,0 +1,13 @@
+package travel.w2m.techproof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacecraftsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacecraftsApplication.class, args);
+	}
+
+}
